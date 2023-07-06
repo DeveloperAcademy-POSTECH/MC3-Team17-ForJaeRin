161518fec8b2a3c4325c0e0406a093a6f44ba44e
yuncoffee <dong072815@gmail.com>
2023-07-06 13:20:24 +0900

    Initial Commit

---
+++ ForJaeRin/ForJaeRinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForJaeRinApp.swift
+//  ForJaeRin
+//
+//  Created by Yun Dongbeom on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct ForJaeRinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
