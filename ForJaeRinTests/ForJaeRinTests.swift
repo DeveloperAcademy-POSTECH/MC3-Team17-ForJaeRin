03ac5fb44f5b3acf13cdb46aa92c9d1a520eba9b
yuncoffee <dong072815@gmail.com>
2023-07-06 17:08:57 +0900

    chore: add swift lint , swift foramt
    .

--- ForJaeRinTests/ForJaeRinTests.swift
+++ ForJaeRinTests/ForJaeRinTests.swift
@@ -5,11 +5,10 @@
 //  Created by Yun Dongbeom on 2023/07/06.
 //
 
-import XCTest
 @testable import ForJaeRin
+import XCTest
 
 final class ForJaeRinTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -23,14 +22,14 @@ final class ForJaeRinTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Mark your test async to allow awaiting for asynchronous code to complete.
+//        Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- ForJaeRinUITests/ForJaeRinUITestsLaunchTests.swift
+++ ForJaeRinUITests/ForJaeRinUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class ForJaeRinUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
