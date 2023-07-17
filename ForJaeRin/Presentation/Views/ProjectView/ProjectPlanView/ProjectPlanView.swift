8fff28b48a14a431a49a48049cea4999eb726de2
yuncoffee <dong072815@gmail.com>
2023-07-17 11:23:59 +0900

    [Chore]: GroupColor 코드 위치 변경 #37

--- ForJaeRin/Presentation/Views/ProjectView/ProjectPlanView/ProjectPlanView.swift
+++ ForJaeRin/Presentation/Views/ProjectView/ProjectPlanView/ProjectPlanView.swift
@@ -7,54 +7,6 @@
 
 import SwiftUI
 
-enum GroupColor: CaseIterable {
-    case groupPurple
-    case groupYellow
-    case groupGreen
-    case groupPink
-    case groupOrange
-    case groupBlue
-    case groupGray
-    
-    var color: Color {
-        switch self {
-        case .groupPurple:
-            return Color.groupPurple
-        case .groupYellow:
-            return Color.groupYellow
-        case .groupGreen:
-            return Color.groupGreen
-        case .groupPink:
-            return Color.groupPink
-        case .groupOrange:
-            return Color.groupOrange
-        case .groupBlue:
-            return Color.groupBlue
-        case .groupGray:
-            return Color.groupGray
-        }
-    }
-    
-    var text: Color {
-        switch self {
-        case .groupPurple:
-            return Color.groupPurpleText
-        case .groupYellow:
-            return Color.groupYellowText
-        case .groupGreen:
-            return Color.groupGreenText
-        case .groupPink:
-            return Color.groupPinkText
-        case .groupOrange:
-            return Color.groupOrangeText
-        case .groupBlue:
-            return Color.groupBlueText
-        case .groupGray:
-            return Color.groupGrayText
-        }
-    }
-}
-
 struct ProjectPlanView: View {
     @State private var leftPaneWidth: CGFloat = 200
     @EnvironmentObject var projectFileManager: ProjectFileManager
