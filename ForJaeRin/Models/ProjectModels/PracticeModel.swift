c327c727920094337147ef3785fa11fe1a9ff1aa
yuncoffee <dong072815@gmail.com>
2023-07-23 23:18:31 +0900

    [Refactor]: ProjectFlowView 코드 정리 및 샘플 데이터 바인딩 #63

--- ForJaeRin/Models/ProjectModels/PracticeModel.swift
+++ ForJaeRin/Models/ProjectModels/PracticeModel.swift
@@ -24,7 +24,7 @@ struct Practice: Identifiable {
 }
 
 let practice1 = Practice(
-    saidKeywords: [["말한 키워드1", "말한 키워드2", "말한 키워드3"], ["말한 키워드1", "말한 키워드2"]],
+    saidKeywords: [["말한 키워드1", "말한 키워드2", "말한 키워드3"], ["말한 키워드1", "말한 키워드2"], [], [], [], [], []],
     speechRanges: [SpeechRange(start: 0, end: 60, group: 0), SpeechRange(start: 61, end: 120, group: 1)],
     progressTime: 300,
     audioPath: URL(string: "ff")!)
