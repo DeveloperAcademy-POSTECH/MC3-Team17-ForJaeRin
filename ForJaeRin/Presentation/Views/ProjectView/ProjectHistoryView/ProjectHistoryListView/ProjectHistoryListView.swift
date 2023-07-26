040279b282704f48094f015b5c1f1088ff829c44
leeyongjun604 <daniel_604@naver.com>
2023-07-26 16:57:24 +0900

    [FEAT] 탭제스쳐 추가완료

--- ForJaeRin/Presentation/Views/ProjectView/ProjectHistoryView/ProjectHistoryListView/ProjectHistoryListView.swift
+++ ForJaeRin/Presentation/Views/ProjectView/ProjectHistoryView/ProjectHistoryListView/ProjectHistoryListView.swift
@@ -14,26 +14,48 @@ struct ProjectHistoryListView: View {
     @StateObject var vm = ProjectHistoryVM()
     @EnvironmentObject var replayVoiceManager: VoiceManager
     @EnvironmentObject var projectFileManager: ProjectFileManager
+    /// 음성 재생될 시점(초)
+    @State var currentTime: CGFloat = 0.0
+    /// 음성 재생중
+    @State var playing: Bool = false
+    @State var timer: Timer?
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Text("첫번째 발표연습 기록")
                 .systemFont(.headline)
             listenAgain()
-                .frame(maxWidth: .infinity, maxHeight: 252)
+                .frame(maxWidth: .infinity)
                 .background(Color.systemWhite)
                 .cornerRadius(12)
         }
+        .onChange(of: currentTime, perform: { _ in
+            if currentTime >= CGFloat(projectFileManager.practices![0].progressTime) {
+                timer?.invalidate()
+                playing = false
+                currentTime = CGFloat(projectFileManager.practices![0].progressTime)
+            } else if currentTime < 0 {
+                currentTime = 0.0
+            }
+        })
         .padding(.horizontal, 72)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-        .border(.blue, width: 2)
     }
 }
 
 extension ProjectHistoryListView {
     // MARK: 구간별 다시듣기
+    /// 재생 중이라면,
+    /// Tap -> 정지, currentTime 이동, 재생
+    /// Drag and Drop
+    /// Drag 시작 -> 정지
+    /// Drop -> currentTime 이동, 재생
+    /// Drop 위치 잘 판별할 것(x축)
+    /// 정지 중이라면,
+    /// currentTime만 옮긴다.(Tap, Drag and Drop 모두)
     private func listenAgain() -> some View {
         VStack(alignment: .leading, spacing: 36) {
+            // 텍스트
             VStack(alignment: .leading, spacing: 8) {
                 Text("구간 별 다시듣기")
                     .font(.systemHeadline)
@@ -42,50 +64,71 @@ extension ProjectHistoryListView {
                     .font(.systemCaption1)
                     .foregroundColor(.systemGray400)
             }
-            .border(.red)
             .padding([.top, .leading], 28)
-            HStack(alignment: .top, spacing: 20) {
-                Image(systemName: "arrowtriangle.forward.fill")
-                    .frame(width: 17.88, height: 20)
-                    .foregroundColor(.systemGray500)
-                    .padding(.top, 53)
+            HStack(alignment: .top, spacing: 16) {
+                // 재생 및 일시정지 버튼
+                Button {
+                    if playing {
+                        replayVoiceManager.pauseRecording()
+                        timer?.invalidate()
+                    } else {
+                        playVoice(time: currentTime)
+                    }
+                    playing.toggle()
+                } label: {
+                    Image(systemName: playing ? "pause.fill" : "play.fill")
+                        .resizable()
+                        .frame(width: 16, height: 20)
+                        .foregroundColor(.systemGray500)
+                        .padding(.top, 53)
+                }.buttonStyle(.plain)
+            Text("00:00")
+                .font(.systemCaption1)
+                .foregroundColor(.systemGray300)
+                .padding(.top, 53)
                 GeometryReader { geometry in
                     let wholeWidthSize = geometry.size.width // 전체 width
-                    ZStack {
+                    ZStack(alignment: .topLeading) {
                         VStack(spacing: 8) {
-                            HStack {
-                                Text("00:00")
-                                    .font(.systemCaption1)
-                                    .foregroundColor(.systemGray300)
-                                Spacer()
-                                Text(vm.secondsToTime(seconds: projectFileManager.practices![0].progressTime))
-                                    .font(.systemCaption1)
-                                    .foregroundColor(.systemGray300)
-                            }
                             speechGroupBlockView(wholeWidthSize: wholeWidthSize)
+                                .onTapGesture(coordinateSpace: .local) { location in
+                                    replayVoiceManager.pauseRecording()
+                                    timer?.invalidate()
+                                    currentTime = locationToTime(
+                                        location: location.x,
+                                        size: wholeWidthSize
+                                    )
+                                    if playing {
+                                        playVoice(time: currentTime)
+                                    }
+                            }
                             speechGroupTextView(wholeWidthSize: wholeWidthSize)
-                        }
-                        .padding(.top, 25)
+                        }.padding(.top, 53)
                         markerView()
-                    }.onTapGesture(coordinateSpace: .local) { location in
-                        print("Tapped at \(location)")
-                        print(Double(location.x) / Double(wholeWidthSize) * Double(projectFileManager.practices![0].progressTime))
-                        replayVoiceManager.playRecording(
-                            audioPath: AppFileManager.shared.directoryPath.appendingPathComponent(gettingAudioPath(path: projectFileManager.practices![0].audioPath),
-                            conformingTo: .mpeg4Audio), time: Double(location.x) / Double(wholeWidthSize) * Double(projectFileManager.practices![0].progressTime))
+                            .padding(.bottom, 17)
+                            .offset(x: timeToOffset(time: currentTime, size: wholeWidthSize))
                     }
                 }
-            }.border(.red)
+                Text(vm.secondsToTime(seconds: projectFileManager.practices![0].progressTime))
+                    .font(.systemCaption1)
+                    .foregroundColor(.systemGray300)
+                    .padding(.top, 53)
+            }
+            .frame(maxHeight: 131)
             .padding(.horizontal, 28)
         }
     }
+    
     private func markerView() -> some View {
         VStack(alignment: .center, spacing: 1) {
-            Text("07:26")
+            Text(vm.secondsToTime(seconds: Int(currentTime)))
                 .font(Font.custom(Pretendard.semibold.fontName, size: 14))
+                .frame(height: 20)
             ZStack(alignment: .top) {
                 Image(systemName: "arrowtriangle.down.fill")
-                    .frame(width: 16, height: 15.55)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 12)
                     .foregroundColor(.systemGray500)
                 Rectangle()
                     .frame(width: 2, height: 56)
@@ -93,12 +136,6 @@ extension ProjectHistoryListView {
                     .padding(.top, 4)
             }
         }
-    }
-    
-    func gettingAudioPath(path: URL) -> String {
-        let sourceURL = path
-        let pdfName = path.absoluteString.components(separatedBy: "/").last!
-        return pdfName
     }
     
     private func speechGroupBlockView(wholeWidthSize: CGFloat) -> some View {
@@ -145,6 +182,7 @@ extension ProjectHistoryListView {
         }
         .frame(maxWidth: wholeWidthSize)
     }
+    
     private func getGroupVolumn(index: Int) -> CGFloat {
         let whole = projectFileManager.practices![0].progressTime // 전체 발표 길이
         var part = 0
@@ -156,6 +194,25 @@ extension ProjectHistoryListView {
             projectFileManager.practices![0].speechRanges[index].start
         }
         return CGFloat(Double(part * 100) / Double(whole))
+    }
+    
+    private func timeToOffset(time: CGFloat, size: CGFloat) -> CGFloat {
+        return Double(time) * Double(size) / Double(projectFileManager.practices![0].progressTime) - 20
+    }
+    
+    private func locationToTime(location: CGFloat, size: CGFloat) -> CGFloat {
+        return Double(projectFileManager.practices![0].progressTime) * Double(location) / Double(size)
+    }
+    
+    private func playVoice(time: CGFloat) {
+        replayVoiceManager.playRecording(audioPath: AppFileManager.shared.directoryPath
+            .appendingPathComponent(vm.gettingAudioPath(path: projectFileManager.practices![0].audioPath),
+                                    conformingTo: .mpeg4Audio), time: time)
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { _ in
+            if playing {
+                currentTime += 0.05
+            }
+        }
     }
 }
 
