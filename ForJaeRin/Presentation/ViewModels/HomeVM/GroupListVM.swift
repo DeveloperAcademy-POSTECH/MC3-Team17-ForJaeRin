281cee32510fcfb2bb17308beaf4c4d4cf2ba5b4
leeyongjun604 <daniel_604@naver.com>
2023-07-31 21:33:38 +0900

    [FIX] done with grouping

--- ForJaeRin/Presentation/ViewModels/HomeVM/GroupListVM.swift
+++ ForJaeRin/Presentation/ViewModels/HomeVM/GroupListVM.swift
@@ -9,7 +9,6 @@ import Foundation
 
 class GroupListVM: ObservableObject {
     /// 해당 그룹 리스트뷰가 수정중인지(최초 제작 혹은 ellipsis를 통해 활성화됨
-    @Published var editMode = true
     @Published var isExpanded = false
     /// editMode가 켜지면 true, trash.fill 혹은 입력을 모두 하지 않고 취소를 누르면 false
     @Published var tempData = ["", "", "", "-1", "-1"]
